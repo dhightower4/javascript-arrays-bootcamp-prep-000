ac1291995f4adf200d3172068989a7f768d4480d
Donte Hightower <dontehightower12@gmail.com>
2017-08-31 20:43:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
-function accesElementInArray(array[element]) {
+
+function accesElementInArray(array) {
   return array[element]
 }
