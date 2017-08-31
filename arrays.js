7c717ec719672b1da8ba4f5f8ed490c880faa530
Donte Hightower <dontehightower12@gmail.com>
2017-08-31 20:58:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop(element)
+  return array.pop()
 }
 
 function removeElementFromEndOfArray(array) {
