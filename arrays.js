2b63520ca10e43cacc705adce2fe8cf596322f6b
Donte Hightower <dontehightower12@gmail.com>
2017-08-31 20:37:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,41 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray([], element) {
-  [...element, []]
+var array = [1]
+var element = 'foo'
+
+function addElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+  return array
 }
-function destructivelyAddElementToBeginningOfArray([], element) {
-  []unshift(element)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+    array = [array, ...element]
+    return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+array = [1, 2, 3]
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
